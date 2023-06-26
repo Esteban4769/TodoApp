9eb6a034050cb5082070d0f8da750f5e08c21780
Vadym Dubniak <dubnyakv@gmail.com>
2023-06-27 02:14:45 +0300

    deploy commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, {
+  useContext, useEffect,
+} from 'react';
+import { UserWarning } from './UserWarning';
+import { ErrorNotification } from './components/ErrorNotification';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { Footer } from './components/Footer';
+import { FilterContext } from './contexts/FilterContext';
+import { TodosContext } from './contexts/TodosContext';
+import { USER_ID } from './constans';
 
 export const App: React.FC = () => {
+  const { filterType } = useContext(FilterContext);
+  const {
+    todos,
+    errorNotification,
+    loadTodos,
+    tempTodo,
+  } = useContext(TodosContext);
+
+  useEffect(() => {
+    loadTodos();
+  }, [filterType]);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        {(!!todos.length || tempTodo) && (
+          <>
+            <Main />
+
+            <Footer />
+          </>
+        )}
+
+      </div>
+
+      {errorNotification && (
+        <ErrorNotification errorNotification={errorNotification} />
+      )}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import { App } from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/index.scss';
+
+import { App } from './App';
+import { FilterContextProvider } from './contexts/FilterContext';
+import { TodosContextProvider } from './contexts/TodosContext';
+
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(
+    <FilterContextProvider>
+      <TodosContextProvider>
+        <App />
+      </TodosContextProvider>
+    </FilterContextProvider>,
+  );
